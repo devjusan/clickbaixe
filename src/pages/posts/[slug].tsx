cada92be53fef1ecc91bc7b20ad3935a38924091
Valmir Santos <jnrplay@hotmail.com>
2022-05-22 01:38:40 -0300

    feat: individual post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,17 @@
+import { StyledSubtitle, StyleTitle } from '@//styles/global';
+import { StyledContainer } from './styles';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
+import img from '../../assets/img.svg';
+
+interface IPost {
+  title: string;
+  subtitle: string;
+  image: string;
+  content: string;
+  updatedAt: string;
+}
 
 const Post: NextPage = () => {
   return (
@@ -7,7 +19,19 @@ const Post: NextPage = () => {
       <Head>
         <title>Posts / CLICKBAIXE</title>
       </Head>
-      <article>so angry</article>
+      <StyledContainer>
+        <StyleTitle
+          css={{ marginBottom: '$44' }}
+          type={{ '@initial': 'title', '@sm': 'mobile' }}
+        >
+          A few words about this blog platform, Ghost, and how this site was
+          made
+        </StyleTitle>
+        <StyledSubtitle css={{ position: 'relative' }} type={'articleTitle'}>
+          Why Ghost (& Figma) instead of Medium, WordPress or other options?
+        </StyledSubtitle>
+        <Image priority={false} src={img} alt="Imagem do post" />
+      </StyledContainer>
     </>
   );
 };
---
+++ src/pages/posts/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from '@//styles';
+
+const StyledContainer = styled('article', {
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$44',
+});
+
+export { StyledContainer };
